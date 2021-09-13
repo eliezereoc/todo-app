e9a6c0030008e29ae2fd9fd680984009119204f7
eliezereoc <eliezeroc@gmail.com>
2021-09-13 08:14:03 -0300

    implementado componentes

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,48 @@
   <div>
     <h1>Todo-app</h1>
   </div>
+  <div v-if="!editMode">
+    <Button @click="newTodo">Novo</Button>
+    <todo-list ></todo-list>
+  </div>
+  <div>     
+    <todo-item v-if="editMode" @cancel="cancel" @salveTodo="salveTodo"></todo-item>
+  </div>
 </template>
 
 <script>
- 
+ import TodoList from './components/TodoList.vue';
+ import TodoItem from './components/TodoItem.vue';
 
 export default {
-  
+  components: {
+    TodoList,
+    TodoItem
+  },
+  data() {
+    return {
+      editMode: false,
+      todos: []
+    }
+  },
+  methods: {
+    newTodo() {
+      this.editMode = true
+    },
+    cancel() {
+      this.editMode = false;
+    },
+    salveTodo(todo) {
+      this.todos.push(todo);
+      localStorage.setItem('todos', JSON.stringify( this.todos));
+    }
+  }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif; 
-  color: #2c3e50;  
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif; 
+    color: #2c3e50;  
+  }
 </style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <h1>Todo-Item</h1>
+  </div>
+
+  <div>
+      <InputText v-model="description"/>
+      <Button @click="salveTodo">Salvar</Button>
+  </div>
+
+  <div>
+    <Button @click="cancel">Voltar</Button>     
+  </div>
+</template>
+
+<script>
+ 
+
+export default {
+  data() {
+      return {
+          description: ""
+      }
+  },
+  methods: {
+      cancel() {
+          this.$emit('cancel');
+      },
+      salveTodo() {
+          this.$emit('salveTodo', {description: this.description});
+      }
+  }
+}
+</script>
+
+<style scoped>
+ 
+</style>
